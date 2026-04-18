ef71247ccff463301432fc19ec81ac14d0133db4
audace011 <karenaudace@gmail.com>
2026-04-18 18:04:55 +0200

    Switched backend from Railway to Render

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://taskbuddy-api-production.up.railway.app/api', // Live Railway Backend URL
+    baseURL: 'https://taskbuddy-api-8d58.onrender.com/api', // Live Render Backend URL
     headers: {
         'Content-Type': 'application/json'
     }
